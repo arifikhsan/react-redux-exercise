29176c3935431bac55b9e10a6cee651e89f934bd
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2022-03-24 08:33:36 +0700

    test build

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('sdfsdf')
+
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
